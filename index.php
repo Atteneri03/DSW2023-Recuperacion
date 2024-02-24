5de109f04f3d5542b46771edb7347116471d73b0
Atteneri <atteeste2003@gmail.com>
2024-02-24 11:42:26 +0000

    complete

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejercicio 2</title>
 </head>
 <body>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
